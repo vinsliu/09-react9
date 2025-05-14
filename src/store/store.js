0c7ce34a90fc8b6e042cd360ae485733834a5da6
Vincent <vincent00lav@outlook.com>
2025-05-14 16:09:19 +0200

    store js update

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // par défaut : localStorage
+import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 import authReducer from "./authSlice";
+
 const rootReducer = combineReducers({
   auth: authReducer,
 });
+
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["auth"], // seules les données du slice auth seront persistées
+  whitelist: ["auth"],
 };
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   reducer: persistedReducer,
 });
